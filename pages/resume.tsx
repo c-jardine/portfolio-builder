2c2dd59acc8935bff1883919cba07337d3e5fd04
Chris Jardine <dev@keplux.com>
2023-01-10 14:12:45 -0500

    Get resume data for resume page and pass down to layout component.

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,8 +1,21 @@
 import { NextPage } from 'next';
 import { ResumeLayout } from '../src/layouts';
+import { sanity } from '../src/studio/client';
+import { getResumeQuery } from '../src/studio/queries';
+import { ResumeType } from '../src/studio/types';
 
-const ResumePage: NextPage = () => {
-  return <ResumeLayout />;
+interface ResumePageProps {
+  resume: ResumeType;
+}
+
+const ResumePage: NextPage = (props: ResumePageProps) => {
+  return <ResumeLayout {...props.resume} />;
 };
+
+export async function getStaticProps() {
+  const resume: ResumeType = await sanity.fetch(getResumeQuery);
+
+  return { props: { resume } };
+}
 
 export default ResumePage;
--- src/layouts/ResumeLayout/ResumeLayout.tsx
+++ src/layouts/ResumeLayout/ResumeLayout.tsx
@@ -1,4 +1,8 @@
-const ResumeLayout = () => {
+import { ResumeType } from '../../studio/types';
+
+type ResumeLayoutProps = ResumeType;
+
+const ResumeLayout = (props: ResumeLayoutProps) => {
   return <div>ResumeLayout</div>;
 };
 export default ResumeLayout;
